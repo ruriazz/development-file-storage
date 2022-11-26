c06cfc9d1648eb6c9f71d0c910344bed98c41dd8
ruriazz <azizruri.s@gmail.com>
2022-11-26 22:28:46 +0700

    change http response code on object_not_found

--- modules/file/services.py
+++ modules/file/services.py
@@ -2,14 +2,14 @@ import os, magic
 from app import settings
 from flask import send_file
 from utils.exceptions import APIException
-from http.client import BAD_REQUEST
+from http.client import NOT_FOUND
 
 class FileService:
     def read_file(object_location: str):
         object_location = os.path.join(settings.BASEPATH, settings.UPLOAD_FOLDER, object_location)
 
         if not os.path.exists(object_location):
-            raise APIException('object_not_found', BAD_REQUEST)
+            raise APIException('object_not_found', NOT_FOUND)
 
         mime = magic.Magic(mime=True)
         return send_file(object_location, mime.from_file(object_location))
